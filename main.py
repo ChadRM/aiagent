57eed454ffc6802441882d354f85a875cd3881ac
ChadRM <chad.r.miller@gmail.com>
2025-12-05 16:44:31 -0600

    think its good...

--- main.py
+++ main.py
@@ -3,31 +3,30 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from prompts import MAIN_PROMPT
+from prompts import system_prompt
+from functions.get_files_info import schema_get_files_info
+
 
 model_name = "gemini-2.5-flash"
-system_prompt = MAIN_PROMPT
 
+print(system_prompt)
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
-# prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
 parser = argparse.ArgumentParser(description="Chadbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
-
 user_prompt = args.user_prompt
 verbose = args.verbose
-
-
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info],
+)
 response = client.models.generate_content(
     model=model_name,
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(system_instruction=system_prompt,tools=[available_functions],),
 )
 if response.usage_metadata != None:
     ptc = response.usage_metadata.prompt_token_count
@@ -41,5 +40,6 @@ if verbose:
     print(f"Response tokens: {ctc}")
 print("Response:")
 print(response.text)
-
-
+if response.function_calls != None:
+    for item in response.function_calls:
+       print(f"Calling function: {item.name}({item.args})")
